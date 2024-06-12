3166520aff284a8166b831278a937d0c8df3ff72
Lean <leandrorodriguez68@gmail.com>
2024-06-12 11:06:21 -0300

    move frontend to backend

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ let notes = [
   },
   {
     id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol.",
+    content: "GET and POST are the most important methods of HTTP protocol",
     important: true
   }
 ]
@@ -32,6 +32,7 @@ const requestLogger = (request, response, next) => {
 app.use(express.json())
 app.use(requestLogger)
 app.use(cors())
+app.use(express.static('dist'))
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
